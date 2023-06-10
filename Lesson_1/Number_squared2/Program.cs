79844e19272c5bb6d24664897142191fc637c56a
Ivanshh86 <ivanshh@yandex.ru>
2023-06-10 09:10:53 +0700

    num_1Sq_2u

---
+++ Lesson_1/Number_squared2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter first number: ");                   // !!!
+ int number_1 = Convert.ToInt32(Console.ReadLine());
+
+ Console.WriteLine("Enter second number: ");
+ int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 == number_2*number_2)
+{
+Console.WriteLine("First number is a square of the second one");
+ }
+ else
+{
+Console.WriteLine("First number is not a square of the second one");
+}

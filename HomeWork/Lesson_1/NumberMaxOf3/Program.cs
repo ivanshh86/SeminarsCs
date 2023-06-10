5feb702cc41dbce527f8dce147f9cec3e867f545
Ivanshh86 <ivanshh@yandex.ru>
2023-06-10 11:53:53 +0700

    AddMaxOf3

---
+++ HomeWork/Lesson_1/NumberMaxOf3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter fist number ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter third number ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.WriteLine("Maximum number " + max);
+

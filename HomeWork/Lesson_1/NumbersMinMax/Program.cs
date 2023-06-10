87e2831374f58379407d14d10738c80af041bbc6
Ivanshh86 <ivanshh@yandex.ru>
2023-06-10 10:52:24 +0700

    AddMaxMin

---
+++ HomeWork/Lesson_1/NumbersMinMax/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter first number  ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter second number  ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max, min;
+
+if (a > b) 
+{
+    max = a;
+    min = b;
+}
+    else
+{   
+    max = b;
+    min = a;
+}
+Console.WriteLine("Maximum number " + max);
+Console.WriteLine("Minimum number " + min);
+
+if (a == b)
+Console.Write("Entered numbers equal " + max);
+Console.WriteLine(" = " + min);

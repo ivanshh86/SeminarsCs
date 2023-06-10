cc3dcdd697e45f4a98486bc2b4d16d8e9d2d3131
Ivanshh86 <ivanshh@yandex.ru>
2023-06-10 13:17:50 +0700

    AddAllEven

---
+++ HomeWork/Lesson_1/NumberN_AllEven/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter number ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while (count <= N)
+{
+    if (count % 2 == 0)
+    Console.WriteLine(count + ", ");
+ 
+count++;
+}

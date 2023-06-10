36d0f58a48bf5e2b466411725c37b9964626c337
Ivanshh86 <ivanshh@yandex.ru>
2023-06-10 09:18:15 +0700

    number_N

---
+++ Lesson_1/Number_N/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter number N: ");
+ int N = Convert.ToInt32(Console.ReadLine());
+ int count = N * (-1);
+ while (count <= N)
+{
+Console.WriteLine(count + ", ");
+count = count + 1; // count +=1; count ++
+}

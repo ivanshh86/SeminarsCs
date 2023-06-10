150456cf6a14c08224a93eb2ebd6e3c862196fdc
Ivanshh86 <ivanshh@yandex.ru>
2023-06-10 09:05:51 +0700

    numSqu

---
+++ Lesson_1/Number_squared/Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.WriteLine("Today is Friday!");
+// Console.Write("Today is Friday!");
+
+ //Console.ReadLine(); // строка
+
+ Console.WriteLine("Enter first number: ");                   // !!!
+ int number = Convert.ToInt32(Console.ReadLine());
+ 
+ int result = number * number;
+
+ Console.WriteLine(result);
+
+ Console.WriteLine($"Entered number is {number}. Squre is {result}");
+
+//Console.WriteLine(number);

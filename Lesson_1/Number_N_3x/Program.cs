9b7e4764ca8edfcea0460b7f878a607c8670b204
Ivanshh86 <ivanshh@yandex.ru>
2023-06-10 09:22:27 +0700

    number_N_3_Digit

---
+++ Lesson_1/Number_N_3x/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Enter number N: ");
+ int N = Convert.ToInt32(Console.ReadLine());
+
+ if (N >99 && N < 1000)
+ 
+// int digit_3 = N % 10;
+// Console.WriteLine(digit_3); 
+{
+Console.WriteLine (+ N % 10);
+ }
+else
+ {
+Console.WriteLine ("Error");
+ }

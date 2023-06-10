ed9f8d463d8846cf9122b453518763efb5016a85
Ivanshh86 <ivanshh@yandex.ru>
2023-06-10 12:12:55 +0700

    AddEvenNumber

---
+++ HomeWork/Lesson_1/NumberEven/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter number ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+Console.WriteLine("Entered number is Even!");
+
+else
+Console.WriteLine("Entered number is Odd!");
+

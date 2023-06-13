9bc664137f2ac0819df6c4ec18990af45e6755e2
Ivanshh86 <ivanshh@yandex.ru>
2023-06-13 20:34:45 +0700

    4-5

--- Lesson_2/5/Program.cs
+++ Lesson_2/5/Program.cs
@@ -1,14 +1,20 @@
 ﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
 
-void crat(int num1, int num2)
+void Crat(int num1, int num2)
 {
-    if (num1 == num2*num2 || num2 == num1*num1)
-{
-    Console.WriteLine("Ok");
+    if (num1 == num2 * num2 || num2 == num1 * num1)
+    {
+        Console.WriteLine("Ok");
+    }
+    else
+    {
+        Console.WriteLine("No");
     }
 }
 
+Console.WriteLine("Enter first number ");
 int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number ");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
-crat(num1, num2);
+Crat(num1, num2);

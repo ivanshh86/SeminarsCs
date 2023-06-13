19bbd0d31bd4dd983894afd7097d8e953a0b514c
Ivanshh86 <ivanshh@yandex.ru>
2023-06-13 22:37:18 +0700

    check2

--- Lesson_2/2/Program.cs
+++ Lesson_2/2/Program.cs
@@ -6,7 +6,6 @@ int num = new Random().Next(100, 1000);
 Console.WriteLine("You number is " + num);
 int dec = num % 10;
 int first = num / 100;
-Console.WriteLine($"{first}  {list}");
+Console.WriteLine($"{first}  {dec}");
 }
-
 Line();

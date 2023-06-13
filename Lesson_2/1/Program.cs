d77834f470241d34fc82e677743c8bc67e3cd7e7
Ivanshh86 <ivanshh@yandex.ru>
2023-06-13 17:20:33 +0700

    1

---
+++ Lesson_2/1/Program.cs
@@ -0,0 +1,15 @@
+﻿int Line(){
+    int num = new Random().Next(10, 100);
+    Console.WriteLine("Your num is " + num);    
+    int dec = num / 10;
+    int ed = num % 10;
+    if(dec > ed){
+        return dec;
+    }
+    else{
+        return ed;
+    }
+      
+}
+
+Console.WriteLine("Result is - " + Line());

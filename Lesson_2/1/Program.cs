6e7134b6087357597d411d4ae7f295986441029a
Ivanshh86 <ivanshh@yandex.ru>
2023-06-13 20:39:27 +0700

    check1

--- Lesson_2/1/Program.cs
+++ Lesson_2/1/Program.cs
@@ -1,15 +1,18 @@
 ﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
 
 
-int Line(){
+int Line()
+{
     int num = new Random().Next(10, 100);
     Console.WriteLine("Your num is " + num);    
     int dec = num / 10;
     int ed = num % 10;
-    if(dec > ed){
+    if(dec > ed)
+    {
         return dec;
     }
-    else{
+    else
+    {
         return ed;
     }
       

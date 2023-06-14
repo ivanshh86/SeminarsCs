eff06d334cade77b33c58fb439d27b1b5ab0ec69
Ivanshh86 <ivanshh@yandex.ru>
2023-06-14 13:59:27 +0700

    dayWeek

---
+++ HomeWork/Lesson_2/DigitDayWeek/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели 
+// и проверяет, является ли этот день выходным.
+
+void Week()
+{
+Console.WriteLine("Введите номер дня недели ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day >= 1 && day <= 5)
+{
+    Console.WriteLine($"{day} - будний день недели");
+}
+if (day >= 6 && day <= 7)
+{
+    Console.WriteLine($"{day} - выходной день недели");
+}
+}
+Week();

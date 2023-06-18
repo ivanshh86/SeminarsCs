e3af1cb2cc9ccd6f15dafc733a8be813a9ff1529
ivanshh86 <ivanshh86@gmail.com>
2023-06-18 10:02:41 +0700

    addPolind

---
+++ HomeWork/Lesson_3/Palindrome/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+
+
+void Polindrom()
+{
+    Console.WriteLine("Enter five-digit number ");
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    int CheckDigit()
+    {
+        int digit = 0;
+        int num = number;
+        while (num !=0)
+        {
+            num /= 10;
+            digit++;
+        }
+        return digit;
+    }
+
+
+
+
+
+
+
+
+
+}

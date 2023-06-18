a2d07cd2a2561ca47777cb7e5bcd16016273d42b
ivanshh86 <ivanshh86@gmail.com>
2023-06-18 16:31:48 +0700

    addCube

--- HomeWork/Lesson_3/Cubes_Numbers/Program.cs
+++ HomeWork/Lesson_3/Cubes_Numbers/Program.cs
@@ -1,4 +1,31 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+void CubeNumN(int numN)
+{
+    int current = 1;
+    while (current <= numN)
+    {
+        double cub = numN * numN * numN;
+        Console.Write($"{cub:f0}, ");           // флаг fx количество (x) символов после запятой        
+        current++;
+    }
 
+}
 
+void CubeNumNNeg(int numN)
+{
+    int current = 1;
+    while (current >= numN)
+    {
+        double cub = Math.Pow(numN, 3);
+        Console.Write(cub + ", ");
+        current--;
+    }
+    Console.WriteLine("\b\b.");                 // удаление 2-х символов справа
+}
+
+Console.WriteLine("Enter number cubed ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0) CubeNumN(number);
+else CubeNumNNeg(number);
--- Lesson_3/5/Program.cs
+++ Lesson_3/5/Program.cs
@@ -9,10 +9,10 @@ void SqurePrinter(int numberN)
     while (current <= numberN)
     {
         double res = Math.Pow(current, 2);
-        Console.WriteLine($"{res:f0}, ");
+        Console.Write($"{res:f0}, ");
         current++;
     }
-    Console.WriteLine("\b\b.");
+    Console.WriteLine("\b\b.");         // удаление двух символов справа
 }
 
 void SqurePrinterNeg(int numberN)
@@ -27,13 +27,13 @@ void SqurePrinterNeg(int numberN)
     Console.WriteLine("\b\b.");
 }
 
-Console.WriteLine("Enter number");
+Console.WriteLine("Enter number ");
 int userNumber = Convert.ToInt32(Console.ReadLine());
 
 if (userNumber > 0)
-SqurePrinter(userNumber);
+    SqurePrinter(userNumber);
 else
-SqurePrinterNeg(userNumber);
+    SqurePrinterNeg(userNumber);
 
 
 

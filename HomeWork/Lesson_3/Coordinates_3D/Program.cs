f3bfd1202971b50adf0ff61d689a3083d63cf559
ivanshh86 <ivanshh86@gmail.com>
2023-06-18 13:50:16 +0700

    coord3D

--- HomeWork/Lesson_3/Coordinates_3D/Program.cs
+++ HomeWork/Lesson_3/Coordinates_3D/Program.cs
@@ -1,4 +1,35 @@
-﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+﻿using System;
+using System.Text.RegularExpressions;
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
+
+
+double LenghtSegment (int xA, int yA, int zA, int xB, int yB, int zB)
+{
+    double lenghtSeg = Math.Sqrt(xB-xA)*(xB-xA)+(yB-yA)*(yB-yA)+(zB-zA)*(zB-zA);
+    lenghtSeg = Math.Round(lenghtSeg, 2);
+    return lenghtSeg;
+}
+
+Console.WriteLine("Enter coordinate x point A");
+int coordXA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter coordinate y point A");
+int coordYA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter coordinate z point A");
+int coordZA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter coordinate x point B");
+int coordXB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter coordinate y point B");
+int coordYB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter coordinate z point B");
+int coordZB = Convert.ToInt32(Console.ReadLine());
+
+double lenght = LenghtSegment(coordXA, coordYA, coordZA, coordXB, coordYB, coordZB);
+Console.WriteLine($"Distance beetwin points A ({coordXA}, {coordYA}, {coordZA}) and B ({coordXB}, {coordYB}, {coordZB}) is {lenght}");
 
 

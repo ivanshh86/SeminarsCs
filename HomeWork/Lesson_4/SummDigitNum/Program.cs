279f7cf6b45c81e8ac3ca628407f48203b79dbfe
ivanshh86 <ivanshh86@gmail.com>
2023-06-21 15:59:39 +0700

    addDigitSumm

---
+++ HomeWork/Lesson_4/SummDigitNum/Program.cs
@@ -0,0 +1,16 @@
+﻿int Number(int num)
+{    
+    int summ = 0;
+    while (num != 0)
+    {
+        summ += num % 10;
+        num /= 10;
+    }
+return summ;
+}
+
+Console.WriteLine("Введите число ");
+int userN = Convert.ToInt32(Console.ReadLine());
+if (userN < 0) userN *= -1;
+Number(userN);
+Console.WriteLine($"Сумма цифр {userN} равна " + Number(userN));

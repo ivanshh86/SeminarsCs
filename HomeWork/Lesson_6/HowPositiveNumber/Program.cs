6803d20239ca7c1d9800ad8d0f5f5107bc25b9e3
ivanshh86 <ivanshh86@gmail.com>
2023-06-30 03:59:23 +0700

    addHWLesson_6

---
+++ HomeWork/Lesson_6/HowPositiveNumber/Program.cs
@@ -0,0 +1,68 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] CreateArray(int numM)
+{
+    int size = numM;
+    int[] numArray = new int[size];
+    for (int i = 0; i < size; i++)
+        numArray[i] = new Random().Next(0, 2147483647);
+    return numArray;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i == array.Length - 1) Console.Write(".");
+        else Console.Write(", ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+void CountPosNum(int[] array)
+{
+    int countNum = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0) countNum++;
+
+    if (countNum >= 11 && countNum <= 19 || countNum % 10 == 5 || countNum % 10 == 6 || countNum % 10 == 7 ||
+        countNum % 10 == 8 || countNum % 10 == 9 || countNum % 10 == 0)
+        Console.WriteLine($"{countNum} чисел больше нуля.");
+
+    else if (countNum % 10 == 2 || countNum % 10 == 3 || countNum % 10 == 4)
+        Console.WriteLine($"{countNum} числа больше нуля.");
+
+    else if (countNum % 10 == 1)
+        Console.WriteLine($"{countNum} число больше нуля.");
+
+}
+
+Console.WriteLine("Укажите размер массива");
+string? sizeNum = Console.ReadLine();
+if (sizeNum == "" || sizeNum == " ")
+{
+    Console.WriteLine($"Введено пустое значение или пробел!");
+    Console.WriteLine();
+}
+else
+{
+    int sizeArray = Convert.ToInt32(sizeNum);
+    if (sizeArray > 0)
+    {
+        int[] userArray = CreateArray(sizeArray);
+        PrintArray(userArray);
+        CountPosNum(userArray);
+    }
+    else if (sizeArray == 0)
+    {
+        Console.WriteLine($"Введено значение {sizeArray}.");
+        Console.WriteLine($"Размер массива не может быть нулевым!");
+    }
+    else
+    {
+        Console.WriteLine($"{sizeArray} отрицательное число.");
+        Console.WriteLine($"Размер массива не может быть отрицательным!");
+    }
+}
---
+++ HomeWork/Lesson_6/LineIntersectionEquation/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+double[] CoordinatArray1(double b1, double k1, double b2, double k2)
+{
+    double x = Math.Round((b2 - b1) / (k1 - k2), 2);
+    double y = Math.Round(k1 * x + b1, 2);
+    double[] coordArray = { x, y };
+    return coordArray;
+}
+double[] CoordinatArray2(double b1, double k1, double b2, double k2)
+{
+    double x = Math.Round((b2 - b1) / (k1 - k2), 2);
+    double y = Math.Round(k2 * x + b2, 2);
+    double[] coordArray = { x, y };
+    return coordArray;
+}
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i == array.Length - 1) Console.Write(".");
+        else Console.Write(", ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+
+Console.Write($"Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write($"Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write($"Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write($"Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine($"Введены значения: b1={b1}, k1={k1}, b2={b2}, k2={k2}");
+
+double[] coordArr1 = CoordinatArray1(b1, k1, b2, k2);
+double[] coordArr2 = CoordinatArray2(b1, k1, b2, k2);
+Console.WriteLine($"Точка пересечения прямых:");
+PrintArray(coordArr1);
+Console.WriteLine($"Проверка расчетов:");
+PrintArray(coordArr2);

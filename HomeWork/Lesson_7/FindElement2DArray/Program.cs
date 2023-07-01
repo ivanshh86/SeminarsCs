4a2cf3281700730f95c533ce434c7bb6ad9dbd29
ivanshh86 <ivanshh86@gmail.com>
2023-07-01 15:56:25 +0700

    addRealNum2dArr

---
+++ HomeWork/Lesson_7/FindElement2DArray/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+double[,] CreateArray2DRealNum(int)
+{
+    double[,] array2dReal = new double[3, 4];
+    for (int i = 0; i < 3; i++)
+        for (int j = 0; j < 4; j++)
+            array2dReal[i, j] = Math.Round(new Random().Next() + new Random().NextDouble(), 2);
+    return array2dReal;
+}
+
+void PrintArray(double[,] array2d)
+{
+    for (int i = 0; i < array2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2d.GetLength(1); j++)
+        {
+            Console.Write(array2d[i, j] + " ");
+        }
+    Console.WriteLine();
+    }
+}
+
+double[,] array2dRealNum = CreateArray2DRealNum();
+PrintArray(array2dRealNum);
---
+++ HomeWork/Lesson_7/RealNum2DArray/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+double[,] CreateArray2DRealNum()
+{
+    Console.Write("Введите минимальное значение массива: ");
+    int min = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите максимальное значение массива: ");
+    int max = Convert.ToInt32(Console.ReadLine());
+    int row = 3;
+    int column = 4;
+    double[,] array2dReal = new double[row, column];
+    for (int i = 0; i < array2dReal.GetLength(0); i++)
+        for (int j = 0; j < array2dReal.GetLength(1); j++)
+            array2dReal[i, j] = Math.Round(new Random().Next(min, max) + new Random().NextDouble(), 2);
+    return array2dReal;
+}
+
+void PrintArray(double[,] array2d)
+{
+    for (int i = 0; i < array2d.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2d.GetLength(1); j++)
+        {
+            Console.Write(array2d[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+double[,] array2dRealNum = CreateArray2DRealNum();
+PrintArray(array2dRealNum);
+

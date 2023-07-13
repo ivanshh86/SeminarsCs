346c6e1ca55650824cba13779d19c07c00a74591
ivanshh86 <ivanshh86@gmail.com>
2023-07-13 12:47:36 +0700

    editLesson_9

--- HomeWork/Lesson_9/SumNumMToN/Program.cs
+++ HomeWork/Lesson_9/SumNumMToN/Program.cs
@@ -4,10 +4,10 @@ int ShowNumbers(int m, int n)
 {
     if (m < 0 && n < 0) return 0;
     // if (m < 0) return (n + 1) * n / 2;
-    // if (n < 0) return (m + 1) * m / 2;
+    // if (n < 0) return (m + 1) * m / 2;                                   // Заменил одной строкой
     if (m < 0 || n < 0) return (Math.Max(m, n) + 1) * Math.Max(m, n) / 2;
     // if (m == n) return n;
-    // if (n > m) return n + ShowNumbers(m, n - 1);             // Заменили одной строкой
+    // if (n > m) return n + ShowNumbers(m, n - 1);                         // Заменил одной строкой
     // return n + ShowNumbers(m, n + 1);
     if (m != n) return Math.Min(n, m) + ShowNumbers(Math.Max(m, n), Math.Min(m, n) + 1);
     return n;

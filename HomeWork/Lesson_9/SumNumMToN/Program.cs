fc97e4903d9e8b322c184e594793abcc4bab8fa8
ivanshh86 <ivanshh86@gmail.com>
2023-07-13 12:51:55 +0700

    editLesson_9

--- HomeWork/Lesson_9/SumNumMToN/Program.cs
+++ HomeWork/Lesson_9/SumNumMToN/Program.cs
@@ -3,12 +3,7 @@
 int ShowNumbers(int m, int n)
 {
     if (m < 0 && n < 0) return 0;
-    // if (m < 0) return (n + 1) * n / 2;
-    // if (n < 0) return (m + 1) * m / 2;                                   // Заменил одной строкой
     if (m < 0 || n < 0) return (Math.Max(m, n) + 1) * Math.Max(m, n) / 2;
-    // if (m == n) return n;
-    // if (n > m) return n + ShowNumbers(m, n - 1);                         // Заменил одной строкой
-    // return n + ShowNumbers(m, n + 1);
     if (m != n) return Math.Min(n, m) + ShowNumbers(Math.Max(m, n), Math.Min(m, n) + 1);
     return n;
 }
@@ -18,3 +13,60 @@ Console.WriteLine("Enter second number: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int sumNum = ShowNumbers(m, n);
 Console.WriteLine($"Sum natural numbers from {m} to {n} is {sumNum}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int ShowNumbers(int m, int n)
+// {
+//     if (m < 0 && n < 0) return 0;
+//     // if (m < 0) return (n + 1) * n / 2;
+//     // if (n < 0) return (m + 1) * m / 2;                                   // Заменил одной строкой
+//     if (m < 0 || n < 0) return (Math.Max(m, n) + 1) * Math.Max(m, n) / 2;
+//     // if (m == n) return n;
+//     // if (n > m) return n + ShowNumbers(m, n - 1);                         // Заменил одной строкой
+//     // return n + ShowNumbers(m, n + 1);
+//     if (m != n) return Math.Min(n, m) + ShowNumbers(Math.Max(m, n), Math.Min(m, n) + 1);
+//     return n;
+// }
